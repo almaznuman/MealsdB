f708fb5018a0270d5858f56c4b54ad2c22a21e6d
Almaz Numan <almaz.20211245@iit.ac.lk>
2023-04-15 11:19:43 +0530

    Update IngredientSearchActivity.kt

--- app/src/main/java/com/example/coursework/IngredientSearchActivity.kt
+++ app/src/main/java/com/example/coursework/IngredientSearchActivity.kt
@@ -10,9 +10,7 @@ import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
-import java.net.UnknownHostException
 
 class IngredientSearchActivity : AppCompatActivity() {
 
